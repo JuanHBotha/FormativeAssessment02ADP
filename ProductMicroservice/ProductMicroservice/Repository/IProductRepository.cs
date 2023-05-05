4f66d2a7f0f4fbb3dda2a8278703d3d016ad42bd
JuanHBotha <111281297+JuanHBotha@users.noreply.github.com>
2023-05-05 16:18:06 +0200

    ADP631 FA2 files added
    FA2 files were added

---
+++ ProductMicroservice/ProductMicroservice/Repository/IProductRepository.cs
@@ -0,0 +1,16 @@
+﻿using ProductMicroservice.Model;
+using System.Collections.Generic;
+namespace ProductMicroservice.Repository
+{
+    public interface IProductRepository
+    {
+        IEnumerable<Product> GetProducts();
+        Product GetProductById(int Productid);
+        void InsertProduct(Product product);
+        void DeleteProduct(Product product);
+        void UpdateProduct(Product product);
+        void Save();
+        object GetProductByID(int id);
+        void DeleteProduct(int id);
+    }
+}
